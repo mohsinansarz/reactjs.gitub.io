df768c5f662ca6b051ddd3bcbc44ec75988ce494
mohsinansarz <32729135+mohsinansarz@users.noreply.github.com>
2020-09-17 10:30:22 +0530

    react
    Reactjs

---
+++ precache-manifest.e54d86161716f885e10dd0a1540ae5c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d569b7065418f3cea8421315f0e613f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4898a02b85a97a8c735c",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2bb95a8156fd617ce2dc",
+    "url": "/static/css/main.3bde81bd.chunk.css"
+  },
+  {
+    "revision": "4898a02b85a97a8c735c",
+    "url": "/static/js/2.20c75e1e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.20c75e1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bb95a8156fd617ce2dc",
+    "url": "/static/js/main.0be32d75.chunk.js"
+  },
+  {
+    "revision": "bff2302db4f741b074af",
+    "url": "/static/js/runtime-main.ada56438.js"
+  }
+]);
